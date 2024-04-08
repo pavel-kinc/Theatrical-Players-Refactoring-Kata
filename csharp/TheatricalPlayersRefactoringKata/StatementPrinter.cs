801223330563063ebd51f32e82dfc7fb564798a3
Pavel Kinc <pavelkinc230@gmail.com>
2024-04-08 13:10:23 +0200

    fix: refactor thisamount

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -12,13 +12,14 @@ namespace TheatricalPlayersRefactoringKata
         {
             public string Name { get; set; }
             public decimal Amount { get; set; }
+
+            public int PlayAmount { get; set; }
             public int Audience { get; set; }
             public int VolumeCredits { get; set; }
         }
 
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
-            var totalAmount = 0;
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
@@ -30,16 +31,17 @@ namespace TheatricalPlayersRefactoringKata
 
                 // print line for this order
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-                totalAmount += thisAmount;
                 list.Add(new PerformanceResultModel
                 {
                     Name = play.Name,
                     Amount = Convert.ToDecimal(thisAmount / 100),
+                    PlayAmount = thisAmount,
                     Audience = perf.Audience,
                     VolumeCredits = CalculateVolumeCredits(perf, play)
                 });
             }
             var volumeCredits = list.Sum(i => i.VolumeCredits);
+            var totalAmount = list.Sum(i => i.PlayAmount);
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
             return result;
